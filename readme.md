e272e95dc783d33a98871a229ab332a295d50c6d
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2022-12-01 17:57:33 +0000

    Add day 1

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Advent of Code 2022
+
+This repo contains my solutions to Advent of Code 2022
+
+This year I can't wake up at 5 AM so I'm not trying to go fastest time to solution, this is an attempt at clean, maintainable code.
+
+Run with:
+
+`cargo run --release -- {day from 1-25}`
+
+Example:
+
+`cargo run --release -- 1`
---
+++ src/day01.rs
@@ -0,0 +1,81 @@
+use crate::runner::Solve;
+
+fn read_input(input: &str) -> Vec<Vec<i32>> {
+    let mut elves = Vec::new();
+    for elf_lines in input.trim().split("\n\n") {
+        let mut elf = Vec::new();
+        for line in elf_lines.split('\n') {
+            let number: i32 = line.parse().unwrap();
+            elf.push(number);
+        }
+        elves.push(elf);
+    }
+    elves
+}
+
+pub struct Day01 {}
+
+impl Solve<i32, i32> for Day01 {
+    fn part1(input: &str) -> i32 {
+        let elves = read_input(input);
+        elves
+            .into_iter()
+            .map(|elf| elf.into_iter().sum())
+            .max()
+            .unwrap()
+    }
+
+    fn part2(input: &str) -> i32 {
+        let elves = read_input(input);
+
+        let mut calories: Vec<i32> = elves.into_iter().map(|elf| elf.into_iter().sum()).collect();
+        calories.sort();
+
+        calories.into_iter().rev().take(3).sum()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r#"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"#;
+
+    #[test]
+    fn parses_example() {
+        let elves = read_input(EXAMPLE);
+        assert_eq!(elves.len(), 5);
+        assert_eq!(elves[0].len(), 3);
+
+        assert_eq!(elves[3][1], 8000);
+    }
+
+    #[test]
+    fn example_p1() {
+        let result = Day01::part1(EXAMPLE);
+        let expected = 24000;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn example_p2() {
+        let result = Day01::part2(EXAMPLE);
+        let expected = 45000;
+
+        assert_eq!(result, expected);
+    }
+}
